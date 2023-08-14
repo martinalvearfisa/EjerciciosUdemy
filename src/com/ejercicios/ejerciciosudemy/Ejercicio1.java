61affde7c6ae8bcb50b599b7b734a3a07291b3a6
martinalvear <martin.alvear@fisagrp.com>
2023-08-14 10:39:08 -0500

    ejercicio 1 y 2

--- src/com/ejercicios/ejerciciosudemy/Ejercicio1.java
+++ src/com/ejercicios/ejerciciosudemy/Ejercicio1.java
@@ -1,15 +1,15 @@
 package com.ejercicios.ejerciciosudemy;
 
 public class Ejercicio1 {
+	public static void main(String[] args) {
+		Ejercicio1 hello = new Ejercicio1();
+		int resultado = hello.hello();
+		System.out.println(resultado);
+	}
+
 	public int hello() {
 		// TODO:Make this return 1
 		return 1;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Ejercicio1 one = new Ejercicio1();
-		int result = one.hello();
-		System.out.println(result);
-	}
 }
