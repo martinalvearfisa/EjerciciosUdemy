a554ad0f715e658d7bf090265e9dbba0b7cdfa30
martinalvear <martin.alvear@fisagrp.com>
2023-08-14 10:32:52 -0500

    Initial commit

---
+++ src/com/ejercicios/ejerciciosudemy/Ejercicio1.java
@@ -0,0 +1,15 @@
+package com.ejercicios.ejerciciosudemy;
+
+public class Ejercicio1 {
+	public int hello() {
+		// TODO:Make this return 1
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ejercicio1 one = new Ejercicio1();
+		int result = one.hello();
+		System.out.println(result);
+	}
+}
